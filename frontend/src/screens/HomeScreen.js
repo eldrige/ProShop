da181da3dc580e2abe1d0e4cb236bf62038fdb45
Apoh Eldrige <peldrige8@gmail.com>
2020-12-12 13:24:48 +0100

    create rating.js component

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -9,7 +9,7 @@ const HomeScreen = () => {
       <h1>Latest Products</h1>
       <Row>
         {products.map((product) => (
-          <Col sm={12} md={6} lg={4} xl={4}>
+          <Col key={product._id} sm={12} md={6} lg={4} xl={4}>
             <Product product={product} />
           </Col>
         ))}

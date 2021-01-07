4385a240a36368b091f04cce858f43347668e821
Apoh Eldrige <peldrige8@gmail.com>
2021-01-07 11:44:38 +0100

    create fake Users

---
+++ backend/data/users.js
@@ -0,0 +1,29 @@
+import bcrypt from 'bcryptjs';
+
+const users = [
+  {
+    name: 'Administrator',
+    email: 'admin@example.com',
+    // the second param specifies the strength of the hash
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: true,
+  },
+  {
+    name: 'John Doe',
+    email: 'johndoe@example.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: true,
+  },
+  {
+    name: 'Jane Doe',
+    email: 'janedoe@example.com',
+    password: bcrypt.hashSync('123456', 10),
+    isAdmin: true,
+  },
+];
+
+// hashing passwords is supposed to be async, but since we r nt
+// dealing with forms yet, we use the hashsync method
+
+
+export default users

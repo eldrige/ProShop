5234f9b09176d354f32963742cd028346b3213ca
Apoh Eldrige <peldrige8@gmail.com>
2021-01-07 11:35:48 +0100

    create models

---
+++ backend/models/orderModel.js
@@ -0,0 +1,91 @@
+import mongoose from 'mongoose';
+
+const orderSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+
+      ref: 'User',
+    },
+    orderItems: [
+      {
+        name: {
+          type: String,
+          require: true,
+        },
+        qty: {
+          type: Number,
+          require: true,
+        },
+        image: {
+          type: String,
+          require: true,
+        },
+        price: {
+          type: Number,
+          require: true,
+        },
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          require: true,
+          ref: 'Product',
+        },
+      },
+    ],
+    shippingAddress: {
+      address: { type: String, required: true },
+      city: { type: String, required: true },
+      postalCode: { type: String, required: true },
+      country: { type: String, required: true },
+    },
+    paymentMethod: {
+      type: String,
+      required: true,
+    },
+    paymentResult: {
+      id: { type: String },
+      status: { type: String },
+      update_time: { type: String },
+      email_address: { type: String },
+    },
+    taxPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    shippingPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    isPaid: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    paidAt: {
+      type: Date,
+    },
+    isDelivered: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    deliveredAt: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true, //this is going to create a time field automatically for all ppties
+  }
+);
+
+const Order = mongoose.model('Order', orderSchema);
+
+export default Order;
---
+++ backend/models/productModel.js
@@ -0,0 +1,73 @@
+import mongoose from 'mongoose';
+
+const reviewSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    rating: { type: Number, required: true },
+    comment: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const productSchema = mongoose.Schema(
+  {
+    // to know which user created which product
+    // this creates a relationship between user and product
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    // another schema into another (nesting/creating relationships)
+    reviews: [reviewSchema],
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    countInStock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true, //this is going to create a time field automatically for all ppties
+  }
+);
+
+const Product = mongoose.model('Product', productSchema);
+
+export default Product;

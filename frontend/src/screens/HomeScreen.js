b9fde3ecdbd78eab1da50f4dae6467c6b38bcc27
Apoh Eldrige <peldrige8@gmail.com>
2021-02-12 00:04:53 +0100

    fetched products using redux

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,30 +1,38 @@
 import React, { useState, useEffect } from 'react';
-// import products from "../products";
+import { useDispatch, useSelector } from 'react-redux';
 import Product from '../components/Product';
 import { Row, Col } from 'react-bootstrap';
-import axios from 'axios';
+import { listProducts } from '../actions/productActions';
+
+// ! firing off the action, then using the selector to get the state
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
+  // *useSelector taks our global state, make sure u use the sm name, defined in your state
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
 
   useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get('/api/products');
-      setProducts(data);
-    };
-    fetchProducts();
-  }, []);
+    // * dispatch calls, our actions
+    dispatch(listProducts());
+  }, [dispatch]);
 
   return (
     <>
       <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={4}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <h1>Loading.....</h1>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={4}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   );
 };

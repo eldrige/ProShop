cdf0ce74965092cb870299efd1699666a395091f
Apoh Eldrige <peldrige8@gmail.com>
2021-02-24 11:04:22 +0100

    user route + controller created

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,15 +1,26 @@
 import asyncHandler from 'express-async-handler';
-import Product from '../models/productModel.js';
+import User from '../models/userModel.js';
 
 // @desc Auth user & get token
 // @route POST /api/users/login
 // @access public
 const authUser = asyncHandler(async (req, res) => {
- const {email,password} = req.body
- res.send({email,password})
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    return res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: null,
+    });
+  } else {
+    // 401 means unauthorized
+    res.status(401);
+    throw new Error('Invalid email or password');
+  }
 });
 
-
-export {
-  authUser
-}
+export { authUser };
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const userSchema = mongoose.Schema(
   {
@@ -25,6 +26,12 @@ const userSchema = mongoose.Schema(
     timestamps: true, //this is going to create a time field automatically for all ppties
   }
 );
+
+// ! mongoose allows us to add methods , which r automatically instantiated to our schemas
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  //  * compare entered password to actual password
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 const User = mongoose.model('User', userSchema);
 

58eb149ba97ec6207dd626ffd6464418f76ea713
Apoh Eldrige <peldrige8@gmail.com>
2021-02-28 19:09:56 +0100

    create userRegister constants and reducers

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -40,6 +40,40 @@ export const login = (email, password) => async (dispatch) => {
   }
 };
 
+export const register = (name,email, password) => async (dispatch) => {
+  try {
+    dispatch({
+      type: USER_REGISTER_REQUEST,
+    });
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    const { data } = await axios.post(
+      '/api/users/',
+      { name, email, password },
+      config
+    );
+
+    dispatch({
+      type: USER_LOGIN_SUCCESS,
+      payload: data,
+    });
+
+    localStorage.setItem('userInfo', JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: USER_LOGIN_FAILURE,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
 export const logout = () => (dispatch) => {
   localStorage.removeItem('userInfo')
   dispatch({type: USER_LOGOUT})
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,6 +7,7 @@ import {
 } from './reducers/productReducer';
 import { cartReducer } from './reducers/cartReducers';
 import { userLoginReducer } from './reducers/userReducers';
+import { userRegisterReducer } from './reducers/userReducers';
 
 // ! whenever you create a reducer, import it to ur store
 const reducer = combineReducers({
@@ -14,6 +15,7 @@ const reducer = combineReducers({
   productDetails: productDetailsReducer,
   cart: cartReducer,
   userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')

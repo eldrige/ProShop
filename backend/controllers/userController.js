e2a740eaea0e462e01ee727ca68a0fdd7f74899e
Apoh Eldrige <peldrige8@gmail.com>
2021-03-07 23:11:45 +0100

    create update user controller for updating user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -37,7 +37,6 @@ const getUserProfile = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      
     });
   } else {
     res.status(404);
@@ -59,7 +58,7 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error('User already exist');
   }
-// create is syntactic sugar, it basiclly acts like a save
+  // create is syntactic sugar, it basiclly acts like a save
   const user = await User.create({
     name,
     email,
@@ -76,10 +75,39 @@ const registerUser = asyncHandler(async (req, res) => {
       token: generateToken(user._id),
     });
   } else {
-    res.status(400)
-    throw new Error("Invalid user data")
+    res.status(400);
+    throw new Error('Invalid user data');
   }
+});
 
+// @desc Update user profile
+// @route PUT /api/users/profile
+// @access Private
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id),
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
 });
 
 export { authUser, getUserProfile, registerUser };

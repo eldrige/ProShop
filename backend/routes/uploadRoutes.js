1ae26100181c3d98fab0b981c1199394faab8306
eldrige <peldrige8@gmail.com>
2021-05-26 16:36:32 +0100

    create upload route

---
+++ backend/routes/uploadRoutes.js
@@ -0,0 +1,45 @@
+// @callBack, takes two params, paramA = err, paramB = Where files will be uploaded
+// extname is a path fn that returns the extension of a file
+import express from 'express';
+import multer from 'multer';
+import path from 'path';
+
+const router = express.Router();
+
+// initializing storage engine
+const storage = multer.diskStorage({
+  // extname() gets the extension of the file
+  destination(req, file, callBack) {
+    callBack(null, 'uploads/');
+  },
+  filename(req, file, callBack) {
+    callBack(
+      null,
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+    );
+  },
+});
+
+const checkFileType = (file, callBack) => {
+  const supportedFileTypes = /jpg|jpeg|png/;
+  const extname = supportedFileTypes.test(
+    path.extname(file.originalname).toLowerCase()
+  );
+  const mimetype = supportedFileTypes.test(file.mimetype);
+
+  if (extname && mimetype) return callBack(null, true);
+  callBack('Images only!');
+};
+
+const upload = multer({
+  storage,
+  fileFilter: function (req, file, callBack) {
+    checkFileType(file, callBack);
+  },
+});
+
+router.post('/', upload.single('image'), (res, res) => {
+  res.send(`/${req.file.path}`);
+});
+
+export default router;

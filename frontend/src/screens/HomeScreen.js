c45628f035eb2dcf537414dbbd78e7dbb675476a
eldrige <peldrige8@gmail.com>
2021-05-31 12:23:36 +0100

    added back button on search

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import Product from '../components/Product';
 import { Row, Col } from 'react-bootstrap';
 import { listProducts } from '../actions/productActions';
@@ -28,7 +29,13 @@ const HomeScreen = ({ match }) => {
   return (
     <>
       <Meta />
-      {!keyword && <ProductCarousel />}
+      {!keyword ? (
+        <ProductCarousel />
+      ) : (
+        <Link to="/" className="btn btn-light">
+          Go Back
+        </Link>
+      )}
       <h1>Latest Products</h1>
       {loading ? (
         <Loader />

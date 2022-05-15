5dd31f981749249e371092422fec37a406a36883
Eldrige <peldrige8@gmail.com>
2022-05-15 16:23:34 +0100

    Modified jwt env key

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,7 +1,9 @@
 import jwt from 'jsonwebtoken';
 
+const JWT_SECRET = '123abc';
+
 const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
+  return jwt.sign({ id }, JWT_SECRET, {
     expiresIn: '30d',
   });
 };

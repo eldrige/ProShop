1a86142c704b2ca6b0acfe8e4ac380e48369048b
Eldrige <peldrige8@gmail.com>
2022-05-31 07:57:24 +0100

    ran yarn build

---
+++ frontend/build/precache-manifest.7b928ff7a888d6066b1e02f72c676720.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70957f51f2efcf773429c1f3d4aeec1b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6204b84cb5c4a829701",
+    "url": "/static/css/2.14ee1281.chunk.css"
+  },
+  {
+    "revision": "5417be9f189a5f5b77b4",
+    "url": "/static/css/main.d52f3821.chunk.css"
+  },
+  {
+    "revision": "d6204b84cb5c4a829701",
+    "url": "/static/js/2.7a2f1caa.chunk.js"
+  },
+  {
+    "revision": "4cf5c6c6f464cc15458a86cbdc669a15",
+    "url": "/static/js/2.7a2f1caa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5417be9f189a5f5b77b4",
+    "url": "/static/js/main.0953fc54.chunk.js"
+  },
+  {
+    "revision": "ce7b350515357ce83319",
+    "url": "/static/js/runtime-main.b56792ae.js"
+  }
+]);
